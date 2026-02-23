5caf5f29e7a88c8ae55017be58b3ead840d7bd40
AtanasI7 <atanas.iwork7@gmail.com>
2026-02-23 12:50:55 +0200

    first commit

---
+++ PDF_Django_ChatBot/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PDF_Django_ChatBot project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PDF_Django_ChatBot.settings')
+
+application = get_wsgi_application()
